f63a17567d1190596e2f9e4c8a7bbfc3bb48751b
KaiiZzeR971 <mrcggwada12@gmail.com>
2019-05-14 21:09:44 -0400

    :)

--- app/src/main/java/com/example/kaiizzer/ppe3/MainActivity.java
+++ app/src/main/java/com/example/kaiizzer/ppe3/MainActivity.java
@@ -6,31 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button sign;
+    private Button login;
+    private SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sessionManager = new SessionManager(this);
+        if(sessionManager.isLogged()){
+            Intent intent = new Intent(this, PremiereActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("REGISTER")){
+            Toast.makeText(this, intent.getStringExtra("REGISTER"), Toast.LENGTH_SHORT).show();
+        }
+
         sign = (Button) findViewById(R.id.sign);
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View V) {
-                openActivity2();
+                openInscription();
             }
         });
-    }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
-    }
-
-    private Button login;
-{
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
                 openConnexion();
             }
         });
+    }
+
+    public void openInscription() {
+        Intent intent = new Intent(this, Inscription.class);
+        startActivity(intent);
     }
 
     public void openConnexion() {
--- app/src/main/java/com/example/kaiizzer/ppe3/Paiement.java
+++ app/src/main/java/com/example/kaiizzer/ppe3/Paiement.java
@@ -3,11 +3,11 @@ package com.example.kaiizzer.ppe3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity2 extends AppCompatActivity {
+public class Paiement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_paiement);
     }
 }
